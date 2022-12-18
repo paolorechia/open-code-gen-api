64f436f6e2904c4a5f91a6a6b137782ad4fef827
paolorechia <paolorechia@gmail.com>
2022-12-18 15:13:39 +0100

    Test API

---
+++ test/test_api.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from open_code_gen_api.main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {'app': 'Open Code Gen API',
+          'help': 'Call POST /prompt to request a response from the model',
+          'model': 'SalesforceCodeGen350M',
+          'version': '0.1'}
+
+def test_prompt():
+    response = client.post("/prompt", json={"prompt_text": "test"})
+    assert response.status_code == 200
+    response = response.json()
+    assert response["model_response"]
